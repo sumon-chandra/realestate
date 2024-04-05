ac96ab8b1d34b3ba5533bd11b645a049cc313a7d
Sumon Chandra <topu199041@gmail.com>
2024-04-05 21:36:01 +0600

    feat: create configuration for env variables

---
+++ src/config/env-variables.ts
@@ -0,0 +1,10 @@
+export const envVariables = {
+	kinde: {
+		clientId: process.env.KINDE_CLIENT_ID,
+		clientSecret: process.env.KINDE_CLIENT_SECRET,
+		issuerUrl: process.env.KINDE_ISSUER_URL,
+		siteUrl: process.env.KINDE_SITE_URL,
+		postLoginDirectedUrl: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
+		postLogoutDirectedUrl: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+	},
+};
