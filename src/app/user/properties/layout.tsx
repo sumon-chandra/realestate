6577004c485b72dda104100a2c606a2adce5d06d
Sumon Chandra <topu199041@gmail.com>
2024-05-13 16:27:25 +0600

    feat(properties): implement intercepting routes on property

--- src/app/user/properties/layout.tsx
+++ src/app/user/properties/layout.tsx
@@ -3,17 +3,17 @@ import { Plus } from "lucide-react";
 import { FC, ReactNode } from "react";
 
 interface Props {
-	children: ReactNode;
-	model: ReactNode;
+  children: ReactNode;
+  model: ReactNode;
 }
 
 const PropertiesLayout: FC<Props> = ({ children, model }) => {
-	return (
-		<>
-			{children}
-			{model}
-		</>
-	);
+  return (
+    <>
+      {children}
+      {model}
+    </>
+  );
 };
 
 export default PropertiesLayout;
